72389a1688406caf3bf440d01ffa2d829c665aeb
Triston Golden <triston_golden@yahoo.com>
2022-01-30 22:00:47 -0500

    coming along

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,23 @@
 const { v4: uuidv4 } = require("uuid");
 const db = require("./db/db.json");
-const exp = require("express");
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const port = 3009;
+const PORT = 3009;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => res.json(db));
+
+app.post("/api/notes", (req, res) => res.json(db));
+
+app.listen(PORT, () =>
+  console.log(`Note Taker app listening at http://localhost:${PORT}`)
+);
