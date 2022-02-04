0a1380e8bd510ee7e4603bcdeaeca0fb10987559
Triston Golden <triston_golden@yahoo.com>
2022-02-04 09:33:32 -0500

    heroku fixes

--- db/store.js
+++ db/store.js
@@ -2,7 +2,7 @@ const util = require("util");
 const fs = require("fs");
 
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const uuidv4 = require("uuid/v4");
+const { v4: uuidv4 } = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
